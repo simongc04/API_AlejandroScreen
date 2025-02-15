367865067b32a98be26bcdd0ad9c5d113ed71219
simongc04 <simonescuela9@gmail.com>
2025-02-15 22:12:09 +0000

    udpate

--- app/src/main/java/com/simon/api_alejandro/network/product/ProductRepository.kt
+++ app/src/main/java/com/simon/api_alejandro/network/product/ProductRepository.kt
@@ -1,19 +1,15 @@
-package com.simon.api_alejandro.network.product
+package com.simon.api_alejandro.network.product;
 
-import android.util.Log
-import com.simon.api_alejandro.network.product.model.ProductListResponse
-import retrofit2.Response
+import com.simon.api_alejandro.network.product.model.ProductListResponse;
 
 class ProductRepository {
-    val api = ProductService()
+    private val api = ProductService();
 
     suspend fun getAllProducts(): ProductListResponse {
         val response = api.getAllProducts()
         if (response.isSuccessful) {
-            Log.d("ProductRepository", "API call successful")
             return response.body() ?: ProductListResponse(emptyList(), 0, 0, 0)
         } else {
-            Log.e("ProductRepository", "API call failed: ${response.errorBody()?.string()}")
             throw Exception("Failed to fetch products")
         }
     }
